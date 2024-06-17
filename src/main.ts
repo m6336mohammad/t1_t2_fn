9a497e4822fd3e8272cf1eefa9603eba3736bded
Mohammad hadi <m6336.programer@gmail.com>
2024-06-17 15:21:48 +0330

    modify from,to,everySecond

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@
 
 import { executeRange, myPrint } from './executeRange.js';
 
-const stepOne: number = 1
-const stepTwo: number = 10
+const from: number = 1
+const to: number = 10
 const everySecond: number = 2
 
-executeRange(stepOne, stepTwo, everySecond, () => {
+executeRange(from, to, everySecond, () => {
     myPrint();
 });
