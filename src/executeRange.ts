2112b9d18d5ccc8178cca83d825ed80fc6d46452
Mohammad hadi <m6336.programer@gmail.com>
2024-06-17 10:05:22 +0330

    create executeRange

---
+++ src/executeRange.ts
@@ -0,0 +1,23 @@
+// executeRange.ts
+export const executeRange = (t1: number, t2: number, everySecond: number, fun: () => void): void => {
+    let current = t1;
+
+    function executeAndScheduleNext() {
+        if (current <= t2) {
+            fun();
+            current += everySecond;
+            setTimeout(executeAndScheduleNext, everySecond * 1000);
+        }
+    }
+
+    executeAndScheduleNext();
+};
+
+// Example function to be executed
+export const myPrint = (): void => {
+    let g = 0;
+    while (g < 5) {
+        console.log("print Nodejs");
+        g += 1;
+    }
+};
