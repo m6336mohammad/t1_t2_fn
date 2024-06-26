2f11716358d142093dbbcf198f358e3b1d4d3111
Mohammad hadi <m6336.programer@gmail.com>
2024-06-26 11:32:11 +0330

    update executeRange.d.ts for type declare

--- dist/executeRange.d.ts
+++ dist/executeRange.d.ts
@@ -1,2 +1,13 @@
-export declare const executeRange: (t1: number, t2: number, everySecond: number, fun: () => void) => void;
-export declare const myPrint: () => void;
+// t1_t2_fn.d.ts
+declare module 't1_t2_fn' {
+    /**
+     * Executes a provided function at intervals of `everySecond` seconds, 
+     * starting at `from` and stopping at `to`.
+     * 
+     * @param from - The starting point in seconds.
+     * @param to - The ending point in seconds.
+     * @param everySecond - The interval at which to execute the function, in seconds.
+     * @param fun - The function to execute.
+     */
+    export function executeRange(from: number, to: number, everySecond: number, fun: () => void): void;
+}
