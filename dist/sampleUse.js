233826f51bf732d9da2e6556bf92904c500a69ab
Mohammad hadi <m6336.programer@gmail.com>
2024-08-12 18:19:36 +0330

    modify

--- dist/sampleUse.js
+++ dist/sampleUse.js
@@ -6,5 +6,5 @@ const from = 1;
 const to = 10;
 const everySecond = 2;
 (0, t1_t2_fn_1.executeRange)(from, to, everySecond, () => {
-    (0, t1_t2_fn_1.myPrint)(); // or console.log("....test....")
+    (0, t1_t2_fn_1.myPrint)(); // or console.log("...test...")
 });
